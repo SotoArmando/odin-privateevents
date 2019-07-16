2b1a2a01f99254406c54423567993520dea4775d
ispirett <ispirett@gmail.com>
2019-07-16 16:27:31 -0400

    feature:(seed data): added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+ User.create(name:Faker::Name.name )
+
+end
+
+15.times do
+  first =  User.first.events.build(title: Faker::Games::Pokemon.name,
+                          description: Faker::Movies::BackToTheFuture.quote, date: 1.month.ago)
+
+  first.save
+end
+
+
+15.times do
+  second = User.second.events.build(title: Faker::Games::Pokemon.name,
+                          description: Faker::Movies::BackToTheFuture.quote, date: 1.month.ago)
+    second.save
+end
